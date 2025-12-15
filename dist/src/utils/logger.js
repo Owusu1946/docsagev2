be162add95f727d3229a534a5dc00225e9ea24f3
Owusu Kenneth <59415541+Owusu1946@users.noreply.github.com>
2025-12-15 22:55:55 +0100

    Delete dist/src directory

--- dist/src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-export const logger = {
-    info: (msg) => console.log(chalk.blue(msg)),
-    success: (msg) => console.log(chalk.green('✔ ' + msg)),
-    error: (msg) => console.error(chalk.red('✘ ' + msg)),
-    warning: (msg) => console.log(chalk.yellow('⚠ ' + msg)),
-    bold: (msg) => console.log(chalk.bold(msg)),
-};
